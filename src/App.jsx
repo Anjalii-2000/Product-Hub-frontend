c8f8f56721c7b0f6bc31a27a0f2827aafd17bcf1
Anjalii-2000 <anjalibhardwaj450@gmail.com>
2026-05-18 07:09:22 +0530

    commented customer dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,10 @@ import Dashboard from "./pages/Dashboard/Dashboard";
 import PrivateRoute from "./Components/PrivateRoute";
 import Seller from './pages/Dashboard/Seller';
 import Customer from './pages/Dashboard/Customer';
-import Wishlist from './Components/CustomerAllComponents/Wishlist';
-import MyOrders from './Components/CustomerAllComponents/MyOrders';
-import Profile from './Components/CustomerAllComponents/Profile';
-import Cart from './Components/CustomerAllComponents/Cart';
+// import Wishlist from './Components/CustomerAllComponents/Wishlist';
+// import MyOrders from './Components/CustomerAllComponents/MyOrders';
+// import Profile from './Components/CustomerAllComponents/Profile';
+// import Cart from './Components/CustomerAllComponents/Cart';
 
 function App() {
   return (
@@ -31,10 +31,10 @@ function App() {
           <Route path='/login' element={<LoginPage />} />
           <Route path='/register' element={<RegisterPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/dashboard/customer/wishlist" element={<Wishlist />} />
+          {/* <Route path="/dashboard/customer/wishlist" element={<Wishlist />} />
           <Route path="/dashboard/customer/myorder" element={< MyOrders />} />
           <Route path="/dashboard/customer/cart" element={<Cart />} />
-          <Route path="/dashboard/customer/profile" element={< Profile />} />
+          <Route path="/dashboard/customer/profile" element={< Profile />} /> */}
           <Route path="/dashboard/seller" element={<PrivateRoute role="seller">
             <Seller />
           </PrivateRoute>
